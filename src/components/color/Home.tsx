28eecf63e18631e78c99d020061fed27915cb8c1
thinhdh04 <tdh855513@gmail.com>
2024-07-23 00:56:40 +0700

    sua file color

--- src/components/color/Home.tsx
+++ src/components/color/Home.tsx
@@ -7,7 +7,7 @@ export const Home = () => {
                 <h1 className='border-4 border-green-600'>
                     Hello World!
                 </h1>
-                <h1 className='text-vuejs'>
+                <h1 className='text-balance'>
                     Hello World!
                 </h1>
             </div>
