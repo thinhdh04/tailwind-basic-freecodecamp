2f5f1103b60fdb5b2da88986c897899e7a615808
thinhdh04 <tdh855513@gmail.com>
2024-07-23 18:30:21 +0700

    hoan thanh typography

---
+++ src/components/typography/Home.tsx
@@ -0,0 +1,16 @@
+
+export const Home = () => {
+    return (
+        <>
+            <div>
+                <p className="break-all">This is a long lknfds,jfdnblkgsfdbglkfgbndfmgjnfdkgdfilksdjfasd.fkjdf sentence with a lot of words to explain that truncate will truncate my sentences</p>
+                <h1 className="text-xl3">Title 1</h1>
+                <h2 className="text-2xl ">Title 2</h2>
+                <h3 className="text-xl">Title 3</h3>
+                <p className="text-base leading-loose underline decoration-bubble-gum decoration-wavy decoration-1 underline-offset-4 uppercase">a regular paragraph</p>
+                <p className="text-sm">A description paragraph</p>
+                <p className="text-xs note">A little note</p>
+            </div>
+        </>
+    )
+}
